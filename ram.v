e83effd89b253ba321e438c1697ea6a610a6d171
Ilya Usoltsev <ilya.usoltsev@zhinst.com>
2026-05-18 15:04:45 +0200

    Fix dout output type and assignment in RAM module

--- ram.v
+++ ram.v
@@ -3,7 +3,7 @@ module ram(
     input wire we,
     input wire [14:0] addr,
     input wire [15:0] din,
-    output reg [15:0] dout
+    output wire [15:0] dout
 );
 
 // spils over the available 4k RAM + ROM
@@ -12,8 +12,8 @@ reg [15:0] mem [0:4095];
 always @(posedge clk) begin
     if (we)
         mem[addr] <= din;
-
-    dout <= mem[addr];
 end
+
+assign dout = mem[addr];
 
 endmodule
